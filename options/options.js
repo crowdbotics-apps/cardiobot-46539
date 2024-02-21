e19c8659099d85f9529f2cc42ae35959b333c618
Crowdbotics <team@crowdbotics.com>
2024-02-21 17:12:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cardiobot-46539",
+  url: "https://cardiobot-46539.botics.co",
+  api: "https://cardiobot-46539.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
